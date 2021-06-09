402c377d112ada821ad8eaca4306cf57a6b66925
Jeff Fessler <fessler@umich.edu>
2021-06-08 22:02:10 -0400

    transA

--- src/ambiguity.jl
+++ src/ambiguity.jl
@@ -5,3 +5,4 @@ This may be a bit of a whack-a-mole™ exercise...
 =#
 
 Base.(*)(A::LinearAlgebra.AbstractTriangular, B::LinearMapAM) = M_AM(A, B)
+Base.(*)(transA::LinearAlgebra.Transpose, B::LinearMapAM) = M_AM(transA, B)
