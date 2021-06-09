5fce81a48ed99ee32836d51fab6401b219cf8f1e
Jeff Fessler <fessler@umich.edu>
2021-06-08 23:04:35 -0400

    resolve ambiguities

--- src/ambiguity.jl
+++ src/ambiguity.jl
@@ -4,5 +4,22 @@ Avoiding method ambiguities
 This may be a bit of a whack-a-mole™ exercise...
 =#
 
-Base.(*)(A::LinearAlgebra.AbstractTriangular, B::LinearMapAM) = M_AM(A, B)
-Base.(*)(transA::LinearAlgebra.Transpose, B::LinearMapAM) = M_AM(transA, B)
+import LinearAlgebra
+
+const Trans = LinearAlgebra.Transpose{<: Any, <: LinearAlgebra.RealHermSymComplexSym}
+const Adjoi = LinearAlgebra.Adjoint{<: Any, <: LinearAlgebra.RealHermSymComplexHerm}
+const Given = LinearAlgebra.Adjoint{<: Any, <: LinearAlgebra.AbstractRotation}
+
+Base.:(*)(A::LinearMapAM, D::LinearAlgebra.Diagonal) = AM_M(A, D)
+Base.:(*)(D::LinearAlgebra.Diagonal, B::LinearMapAM,) = M_AM(D, B)
+Base.:(*)(A::LinearMapAM, B::LinearAlgebra.AbstractTriangular) = AM_M(A, B)
+Base.:(*)(A::LinearAlgebra.AbstractTriangular, B::LinearMapAM) = M_AM(A, B)
+Base.:(*)(A::LinearMapAM, B::Trans) = AM_M(A, B)
+Base.:(*)(A::Trans, B::LinearMapAM) = M_AM(A, B)
+Base.:(*)(A::LinearMapAM, B::Adjoi) = AM_M(A, B)
+Base.:(*)(A::Adjoi, B::LinearMapAM) = M_AM(A, B)
+Base.:(*)(A::LinearMapAM, B::Given) = AM_M(A, B)
+Base.:(*)(A::Given, B::LinearMapAM) = M_AM(A, B)
+
+Base.:(*)(x::LinearAlgebra.AdjointAbsVec, A::LinearMapAM) = (A'*x')'
+Base.:(*)(x::LinearAlgebra.TransposeAbsVec, A::LinearMapAM) = transpose(transpose(A)*transpose(x))
