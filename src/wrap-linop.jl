c6d19a951a635b1ede2e32d0184f8854ce220257
Jeff Fessler <fessler@umich.edu>
2022-01-09 10:03:21 -0500

    wrap other way

--- src/wrap-linop.jl
+++ src/wrap-linop.jl
@@ -1,9 +1,11 @@
 # wrap-linop.jl
-# Wrap a LinearMapAA around a LinearOperator
-
-export LinearMapAA
+# Wrap a LinearMapAA around a LinearOperator or vice-versa
 
 using .LinearOperators: LinearOperator
+import .LinearOperators: LinearOperator
+
+export LinearMapAA
+export LinearOperator
 
 
 """
@@ -16,4 +18,28 @@ function LinearMapAA(L::LinearOperator ; kwargs...)
     forw!(y, x) = mul!(y, L, x)
     back!(x, y) = mul!(x, L', y)
     return LinearMapAA(forw!, back!, size(L); kwargs...)
+end
+
+
+"""
+    L = LinearOperator(A::LinearMapAX; symmetric, hermitian)
+
+Wrap a `LinearOperator` around a `LinearMapAX`.
+The options `symmetric` and `hermitian` are `false` by default.
+"""
+function LinearOperator(
+    A::LinearMapAX;
+    symmetric::Bool = false,
+    hermitian::Bool = false,
+)
+    forw!(y, x) = mul!(y, A, x)
+    back!(x, y) = mul!(x, A', y)
+    return LinearOperator(
+        eltype(A),
+        size(A)...,
+        symmetric, hermitian,
+        forw!,
+        nothing, # transpose mul!
+        back!,
+    )
 end
