85bf1cd8a3928e071c849f533873d046a01d8bc8
Jeffrey A. Fessler <fessler@umich.edu>
2022-02-01 21:38:54 -0500

    test cuda

--- example/cuda.jl
+++ example/cuda.jl
@@ -1,12 +1,17 @@
 #=
 example/cuda.jl
 Verify that this package works with CUDA arrays.
+(This code can run only on a system with a NVidia GPU.)
 =#
 
 using CUDA: cu
+import CUDA: allowscalar
 using FFTW: fft, bfft, fft!, bfft!
 using LinearMapsAA: LinearMapAA
 using LinearAlgebra: mul!
+
+allowscalar(false) # ensure no scalar operations
+
 
 N = 8
 T = ComplexF32
@@ -17,10 +22,42 @@ x = randn(T, N)
 y = A * x
 
 xc = cu(x)
+yc = A * xc
 
-#=
-if true # essentially identical
-    @btime mul!($y, $L, $x)
-    @btime mul!($y, $A, $x)
-end
-=#
+@assert Array(yc) ≈ y
+
+bc = A' * yc
+b = A' * y
+@assert Array(bc) ≈ b
+
+
+M,N = 16,8
+T = ComplexF32
+forw!(y,x) =  fft!(copyto!(y,x))
+back!(x,y) = bfft!(copyto!(x,y))
+B = LinearMapAA(forw!, back!, (N*M, N*M), (name="fft",); idim = (M,N), odim = (M,N), T)
+@assert Matrix(B') == Matrix(B)'
+
+
+# test fft
+
+x = randn(T, M, N)
+y = similar(x)
+mul!(y, B, x)
+
+xc = cu(x)
+yc = similar(xc)
+mul!(yc, B, xc)
+
+@assert Array(yc) ≈ y
+
+
+# test bfft
+
+b = similar(x)
+mul!(b, B', y)
+
+bc = similar(yc)
+mul!(bc, B', yc)
+
+@assert Array(bc) ≈ b
