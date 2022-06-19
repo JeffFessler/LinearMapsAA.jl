b752ae01fe02a782577d524abed531af17354f7c
Jeff Fessler <fessler@umich.edu>
2022-06-19 02:37:08 -0500

    no scalar getindex

--- test/getindex.jl
+++ test/getindex.jl
@@ -4,6 +4,11 @@
 using LinearMapsAA: LinearMapAA, LinearMapAX
 using Test: @test, @testset
 
+# only slicing supported as of LM 3.7 !
+_slice(a::Colon, b::Int) = true
+_slice(a::Int, b::Colon) = true
+_slice(a::Colon, b::Colon) = true
+_slice(a::Any, b::Any) = false
 
 """
     LinearMapAA_test_getindex(A::LinearMapAX)
@@ -17,8 +22,8 @@ function LinearMapAA_test_getindex(A::LinearMapAX)
     tf2 = [false; trues(size(A,2)-2); false]
     ii1 = (3, 2:4, [2,4], :, tf1)
     ii2 = (2, 3:4, [1,4], :, tf2)
-    for i2 in ii2
-        for i1 in ii1
+    for i2 in ii2, i1 in ii1
+        if _slice(i1, i2)
             @test B[i1,i2] == A[i1,i2]
         end
     end
@@ -26,13 +31,17 @@ function LinearMapAA_test_getindex(A::LinearMapAX)
     L = A._lmap
     test_adj = !((:fc in propertynames(L)) && isnothing(L.fc))
     if test_adj
-        for i1 in ii2
-            for i2 in ii1
+        for i1 in ii2, i2 in ii1
+            if _slice(i1, i2)
                 @test B'[i1,i2] == A'[i1,i2]
             end
         end
     end
+    true
+end
 
+
+function LinearMapAA_test_getindex_scalar(A::LinearMapAX)
     # "end"
     @test B[3,end-1] == A[3,end-1]
     @test B[end-2,3] == A[end-2,3]
@@ -51,7 +60,7 @@ function LinearMapAA_test_getindex(A::LinearMapAX)
     kk = [k in [3,5] for k = 1:length(A)] # Bool
     @test B[kk] == A[kk]
 
-    # Some tests could rely on the fact that LinearMapAM <:i AbstractMatrix,
+    # Some tests could rely on the fact that LinearMapAM <: AbstractMatrix,
     # by inheriting from general Base.getindex, but all are provided here.
     @test B[[1, 3, 4]] == A[[1, 3, 4]]
     @test B[4:7] == A[4:7]
@@ -65,3 +74,4 @@ forw = x -> [cumsum(x); 0; 0]
 back = y -> reverse(cumsum(reverse(y[1:N])))
 A = LinearMapAA(forw, back, (M, N))
 @test LinearMapAA_test_getindex(A)
+# @test LinearMapAA_test_getindex_scalar(A) # no!
