97b2518139753a238bec1bb84b6d3a7100c1a35f
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-06-17 10:14:29 -0400

    add aqua tests (#75)

--- test/aqua.jl
+++ test/aqua.jl
@@ -3,5 +3,9 @@ import Aqua
 using Test: @testset
 
 @testset "aqua" begin
-    Aqua.test_all(LinearMapsAA)
+    Aqua.test_all(
+        LinearMapsAA;
+        deps_compat = (; ignore = [:LinearAlgebra, :SparseArrays]),
+        piracies = false, # todo: see src/identity
+    )
 end
